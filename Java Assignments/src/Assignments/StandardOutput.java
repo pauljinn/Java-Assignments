6a018edced2deb1d5ab73709c097f2b3bb4bfd89
aman soni <soniaman360@gmail.com>
2021-12-14 12:19:54 +0530

    First Assignment Hello World

---
+++ Java Assignments/src/Assignments/StandardOutput.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class StandardOutput {
+	
+	public static void printGiven(String printStatement) {
+		System.out.println(printStatement);
+	}
+	public static void main(String[] args) {
+		StandardOutput.printGiven("Hello World");
+		StandardOutput.printGiven("How are you ?");
+
+	}
+
+}
